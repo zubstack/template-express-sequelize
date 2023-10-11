60948e3db13fdc5c97b87ec89ed234062e9d1a08
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2023-10-11 17:23:50 -0500

    [initial]: express app

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ## List of dependencies:
 
 - npm i nodemon eslint eslint-config-prettier eslint-plugin-prettier pritter -D
+- npm i express
--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello");
+const express = require('express');
+const app = express();
+const port = 4000;
+
+app.get('/', (req, res) => {
+  res.send({
+    content: 'Bla',
+    important: false,
+    createdAt: '00/00/00',
+  });
+});
+
+app.listen(port, () => {
+  console.log('My port: ' + port);
+});
