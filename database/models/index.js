9c96786c5c49daab614f955708b46fca53130293
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2024-01-09 19:46:53 -0500

    main entities

--- database/models/index.js
+++ database/models/index.js
@@ -1,20 +1,12 @@
-const { User, UserSchema } = require('./user.model');
 const { Post, PostSchema } = require('./post.model');
 const { Category, CategorySchema } = require('./category.model');
-const { Ebook, EbookSchema } = require('./ebook.model');
-const { Order, OrderSchema } = require('./order.model');
 
 function setupModels(sequelize) {
-  User.init(UserSchema, User.config(sequelize));
   Post.init(PostSchema, Post.config(sequelize));
   Category.init(CategorySchema, Category.config(sequelize));
-  Ebook.init(EbookSchema, Ebook.config(sequelize));
-  Order.init(OrderSchema, Order.config(sequelize));
 
   Post.associate(sequelize.models);
   Category.associate(sequelize.models);
-  Order.associate(sequelize.models);
-  User.associate(sequelize.models);
 }
 
 module.exports = setupModels;
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,11 @@
 const express = require('express');
 const postsRouter = require('../routes/posts.router');
 const categoriesRouter = require('../routes/categories.router');
-const usersRouter = require('../routes/users.router');
-const ebooksRouter = require('./ebooks.router.js');
-const ordersRouter = require('./orders.router.js');
 
 function routerApi(app) {
   const router = express.Router();
   app.use('/api/v1', router);
   router.use('/posts', postsRouter);
   router.use('/categories', categoriesRouter);
-  router.use('/users', usersRouter);
-  router.use('/ebooks', ebooksRouter);
-  router.use('/orders', ordersRouter);
 }
-
 module.exports = routerApi;
