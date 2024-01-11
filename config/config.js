e45a9938cdd4a743f821a4ae7bb52e11bb6c0f55
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2024-01-11 10:02:38 -0500

    create testing db with migration

--- config/config.js
+++ config/config.js
@@ -13,6 +13,7 @@ const config = {
   dbHost: process.env.DB_HOST,
   dbPort: process.env.DB_PORT,
   dbName: database,
+  dbTesting: process.env.DB_NAME_TEST,
   testing: process.env.NODE_ENV === 'test',
 };
 
--- database/config.js
+++ database/config.js
@@ -1,16 +1,16 @@
 const { config } = require('../config/config');
-
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = (database) =>
+  `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${database}`;
 
 module.exports = {
   development: {
-    url: URI,
+    url: URI(config.dbName),
     dialect: 'postgres',
   },
   test: {
-    url: URI,
+    url: URI(config.dbTesting),
     dialect: 'postgres',
   },
   production: {
